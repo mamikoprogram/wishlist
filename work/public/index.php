abc4de38bfb547ffb6dac3c4da651354574851bd
mamiko <tokunaga8950@gmail.com>
2022-02-27 11:07:30 +0900

    #8 wish作成画面作業開始

--- work/public/index.php
+++ work/public/index.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Wish List</title>
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+  <main>
+    <h1>Wish List</h1>
+    <button>Wishを追加</button>
 
+  </main>
+  <script src="main.js"></script>
 </body>
 </html>
